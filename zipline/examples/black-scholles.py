546a511af12a57ee3a93a303d881015d431d6ff8
Reef Squid <pmavr002@odu.edu>
2023-03-11 02:23:43 -0700

    Update
    Added Black-Scholles

---
+++ zipline/examples/black-scholles.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.stats import norm
+
+#dec var
+r = 0.01 # interest rate
+S =30 #underlying
+K = 40 #strike
+T = 240/365 #theta
+sigma = 0.30 #volatility, vega or kappa?
+
+def blackScholes(r, S, K, T, sigma, type="c"):
+    "Calculate BS price of call/put"
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type == "c":
+            price = S*norm.cdf(d1, 0, 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+        elif type == "p":
+            price = K*np.exp(-r*T)*norm.cdf(-d2, 0, 1) - S*norm.cdf(-d1, 0, 1)
+        return price
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
